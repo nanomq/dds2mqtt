578046a94ce0ecf546b2124318f862745899dabc
wanghaemq <wanghamax@gmail.com>
2022-12-16 10:52:49 +0800

    * NEW [cyclone] Change the implementation of Idl2Mqtt.

--- cyclonedds/HelloWorldMQTTTypes.c
+++ cyclonedds/HelloWorldMQTTTypes.c
@@ -1,15 +1,13 @@
 #include <string.h>
 
-#include <iostream>
-#include <string>
-
 #include "HelloWorldMQTTTypes.h"
+#include "HelloWorld.h"
 
 void
 HelloWorld_to_MQTT(HelloWorld *m1, fixed_mqtt_msg *m2)
 {
-	const char *data = m1->message().c_str();
-	int sz = m1->message().size();
+	const char *data = m1->message;
+	int sz = strlen(m1->message);
 
 	m2->payload = (uint8_t *)data;
 	m2->len = sz;
@@ -21,6 +19,6 @@ MQTT_to_HelloWorld(fixed_mqtt_msg *m1, HelloWorld *m2)
 	char *data = (char *)m1->payload;
 	int   len  = m1->len;
 
-	m2->message(std::string(data).substr(0, len));
+	m2->message = strndup(data, len);
 }
 
